35c7a386956ce9c4c553cc6642af6a3e4292da0f
bibek_msd <bibekgautam318@gmail.com>
2026-05-05 07:42:47 +0000

    added sudo for temp files

--- modules/cleanup.sh
+++ modules/cleanup.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
 system_cleanup(){
-	   echo "===== CLEANUP ====="
-	   read -p  "Find unused temp files for n days(leave blank for all time) :" N
+    echo "===== CLEANUP ====="
+    read -p "Find unused temp files for n days (leave blank for all time): " N
 
-
-     N_DAYS=${N:-0}
-echo "Targeting files older than $N_DAYS days..."
-    find /tmp -type f -atime +$N_DAYS -print
-
+    N_DAYS=${N:-0}
+    echo "Targeting files older than $N_DAYS days..."
    
-  read -p "Confirm deletion? (y/n): " confirm
-if [[ $confirm == [yy]* ]] ;then
-	find /tmp -type f -atime +$N_DAYS -delete
-    echo "Files deleted."
-else
-	echo "Cleanup Cancelled"
-fi
+   sudo find /tmp -type f -atime +$N_DAYS 2>/dev/null -print
+   
+    read -p "Confirm deletion? (y/n): " confirm
+    if [[ $confirm == [yY]* ]]; then
+       sudo find /tmp -type f -mtime +$N_DAYS 2>/dev/null -delete
+        echo "Files deleted."
+    else
+        echo "Cleanup Cancelled"
+    fi
 }
-
